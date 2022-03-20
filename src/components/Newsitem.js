02e1906256e3768d81b8290d59f2755420bb20ea
Shailesh <shaileshtripathi511@gmail.com>
2022-03-20 18:13:05 +0530

    added Badge on card

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react'
 
 export default class Newsitem extends Component {
-    render() {
+  render() {
 
-        const {title,description,imgUrl,newsUrl,source, date,author}=this.props;
-        return (
-            <div className="card">
-            <img src={imgUrl} className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}</p>
-              <p class="card-text"><small class="text-muted">by {!author?"unknown":author} on {new Date(date).toUTCString()}</small></p>
+    const { title, description, imgUrl, newsUrl, source, date, author } = this.props;
+    return (
+      <div className="card">
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+         {source}
+        </span>
 
-              <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read more</a>
-            </div>
-          </div>
-        )
-    }
+        <img src={imgUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p class="card-text"><small class="text-muted">by {!author ? "unknown" : author} on {new Date(date).toUTCString()}</small></p>
+
+          <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read more</a>
+        </div>
+      </div>
+    )
+  }
 }
